a528c6a0d15408411e407d94fec1e10bfc5b5002
Bárbara Oyarzo <163349757+BABA1111111@users.noreply.github.com>
2024-04-25 03:19:48 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -41,7 +41,7 @@ int is_equal(void* key1, void* key2){
 
 void insertMap(HashMap * map, char * key, void * value) {
   Pair* nuevoPar;
-  nuevoPar = (Par*) malloc (sizeof(Par));
+  nuevoPar = (Pair*) malloc (sizeof(Par));
 
   nuevoPar->key = key;
   nuevoPar->value = value;
