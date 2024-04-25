e1ce8951da763c842407168eca6f5b76a49ecc2c
Bárbara Oyarzo <163349757+BABA1111111@users.noreply.github.com>
2024-04-25 04:04:13 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -98,14 +98,14 @@ Pair * searchMap(HashMap * map,  char * key) {
   
   long valorHash = hash(key, map->capacity);
   
-  while(strcmp(map->buckets[valorHash]->key, key) != 0 && map->buckets[valorHash] != NULL){
+  while(strcmp(map->buckets[valorHash]->key, key) != 0){
     if (valorHash == map->capacity - 1){
       valorHash = 0;
     }
     else{
-      //valorHash++;
+      valorHash++;
     }
-    valorHash++;
+    //valorHash++;
   }
   
   
