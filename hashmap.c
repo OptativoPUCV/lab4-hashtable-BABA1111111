3c13b25803e897b86c57368492c0eadab6d79dfe
Bárbara Oyarzo <163349757+BABA1111111@users.noreply.github.com>
2024-04-25 02:34:21 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -68,9 +68,9 @@ HashMap * createMap(long capacity) {
   HashMap* mapa = malloc (sizeof(HashMap));
   mapa->buckets = (Pair**) malloc(sizeof(Pair*) * capacity);
   
-  mapa->size = NULL;
+  mapa->size = 0;
   mapa->capacity = capacity;
-  mapa->current = NULL;
+  mapa->current = 0;
 
   for (int i = 0; i < capacity; i++){
     mapa->buckets[i] = NULL;
