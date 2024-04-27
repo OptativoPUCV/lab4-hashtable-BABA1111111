3997411d4c3719533103837a53bfd47b627cde70
Bárbara Oyarzo <163349757+BABA1111111@users.noreply.github.com>
2024-04-27 20:03:21 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -85,20 +85,13 @@ HashMap * createMap(long capacity) {
 
 void eraseMap(HashMap * map,  char * key) {    
   long valorHash = hash(key, map->capacity);
-  if (strcmp(map->buckets[valorHash]->key,key)==0){
-    map->buckets[valorHash]->key = NULL;
-  }
-  else{
-  while(map->buckets[valorHash] != NULL){
+
+  while(map->current != NULL){
     if (strcmp(map->buckets[valorHash]->key,key)==0){
-      map->current = NULL;
       map->buckets[valorHash]->key = NULL;
+      map->size--;
+      break;
     }
-    if (valorHash == map->capacity - 1){
-      valorHash = 0;
-    }
-    valorHash++;
-  }
   }
   
 }
