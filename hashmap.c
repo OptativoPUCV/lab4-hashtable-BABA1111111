13c2a5450f39aa2491b04e991a7648147be081ba
Bárbara Oyarzo <163349757+BABA1111111@users.noreply.github.com>
2024-04-27 22:40:06 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -153,7 +153,7 @@ Pair * nextMap(HashMap * map) {
     for (long i = map->current; i < map->capacity; i++){
       if (map->buckets[i] != NULL && map->buckets[i]->key != NULL){
         map->current = i + 1;
-        return map->buckets[i];
+        return map->buckets[i + 1];
       }
     }
   }
