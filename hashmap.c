5ffdeb25943bf550643fee7350da99e8bdee957e
Bárbara Oyarzo <163349757+BABA1111111@users.noreply.github.com>
2024-04-27 22:08:31 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -134,13 +134,14 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-  if (map->size != 0 && map->current != -1){
+  
+  // if (map->size != 0 && map->current != -1){
     for (long i = 0; i < map->capacity; i++){
       if (map->buckets[i] != NULL){
         return map->buckets[i];
       }
     }
-  }
+  // }
 
   return NULL;
 }
